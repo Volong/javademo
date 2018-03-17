cb78e5d3c4a3c62803da6167ad7880fc1167c4a1
volong <yhl506481593@qq.com>
2018-03-17 23:29:40 +0800

    sort

---
+++ src/main/java/algorithm/sort/BubbleSort.java
@@ -0,0 +1,36 @@
+package algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ * 依次比较相邻的元素，如果前一个元素大于后一个元素则交换位置
+ * 所以每次比较完就会找出一个最大的元素
+ * 
+ * 时间复杂度：O(n^2)
+ * 空间复杂度:O(1)
+ * 
+ * @author volong
+ *
+ */
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {2, 6, 0, 8, 4, -1};
+		
+		for (int i = 0; i < arr.length -1; i++) {
+			
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(arr));
+		
+	}
+}
--- src/main/java/algorithm/sort/QuickSort.java
+++ src/main/java/algorithm/sort/QuickSort.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 
 /**
  * 快速排序
+ * 选取一个基准值，小于基准值的元素放基准值的左边，大于基准值的元素防基准值的右边
+ * 对小于/大于基准值的元素再执行如上步骤
  * 
+ * 时间复杂度：O(nlogn)
+ * 空间复杂度：O(1)
  * @date 2018年3月16日 下午4:27:27
  *
  */
@@ -14,29 +18,56 @@ public class QuickSort {
         
         int[] arr = {2, 6, 0, 8, 4, -1};
         
-        quickSort(arr, arr.length, 0);
+        int[] quickSort = quickSort(arr);
         
-        System.out.println(Arrays.toString(arr));
-    }
-    
-    public static void quickSort(int[] arr, int length, int pivotIndex) {
-        
-        if (length < 2) {
-            return;
-        }
-        
-        int pivot = arr[pivotIndex];
-        for (int i = 0; i < length; i++) {
-            if (arr[i] > arr[pivotIndex]) {
-                int temp = arr[i];
-                arr[i] = pivot;
-                arr[pivotIndex] = temp;
-                pivotIndex = i;
-            }
-        }
-        quickSort(arr, pivotIndex + 1, 0);
-        quickSort(arr, length - pivotIndex, pivotIndex);
+        System.out.println(Arrays.toString(quickSort));
         
     }
 
+    public static int[] quickSort(int[] arr) {
+    	
+    	if (arr.length < 2) {
+    		return arr;
+    	}
+    	
+    	int pivot = arr[0];
+    	int[] lowArr = new int[arr.length];
+    	int[] highArr = new int[arr.length];
+    	int lowIndex = 0;
+    	int highIndex = 0;
+    	
+    	for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < pivot) {
+				lowArr[lowIndex] = arr[i];
+				lowIndex++;
+			} else if (arr[i] > pivot) {
+				highArr[highIndex] = arr[i];
+				highIndex++;
+			}
+		}
+    	
+    	int[] lowSort = quickSort(Arrays.copyOf(lowArr, lowIndex));
+		int[] highSort = quickSort(Arrays.copyOf(highArr, highIndex));
+		
+		return merge(lowSort, pivot, highSort);
+    }
+
+    public static int[] merge(int[] lowArr, int pivot, int[] highArr) {
+    	
+    	int lowIndex = lowArr.length;
+    	int highIndex = highArr.length;
+    	
+    	int[] mergeArr = new int[lowIndex + highIndex + 1];
+    	
+    	for (int i = 0; i < lowArr.length; i++) {
+			mergeArr[i] = lowArr[i];
+		}
+    	mergeArr[lowIndex] = pivot;
+    	
+    	for (int i = 0; i < highArr.length; i++) {
+			mergeArr[lowIndex + 1 + i] = highArr[i];
+		}
+    	
+    	return mergeArr;
+    }
 }
