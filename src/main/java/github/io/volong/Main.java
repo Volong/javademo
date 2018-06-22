9d255b05c8cd83d6accbf3e8b40485aa5c0e0630
Volong <yhlona523@gmail.com>
2018-06-22 21:21:08 +0800

    thread

--- src/main/java/github/io/volong/Main.java
+++ src/main/java/github/io/volong/Main.java
@@ -3,7 +3,19 @@ package github.io.volong;
 public class Main {
 
     public static void main(String[] args) {
-        double x = 1.0 / 0.0;
-        System.out.println(new Double(x).equals("Infinity"));
+    	System.out.println((1 << 31) -1);
+    	System.out.println(0x7fffffff);
+    }
+    
+    public static final int hash(Object k) {
+        int h = 0;
+
+        h ^= k.hashCode();
+
+        // This function ensures that hashCodes that differ only by
+        // constant multiples at each bit position have a bounded
+        // number of collisions (approximately 8 at default load factor).
+        h ^= (h >>> 20) ^ (h >>> 12);
+        return h ^ (h >>> 7) ^ (h >>> 4);
     }
 }
