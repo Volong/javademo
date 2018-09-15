d041593b8c5ecea74fe264eae7bfceb4656f699e
Volong <yhlona523@gmail.com>
2018-09-15 16:36:05 +0800

    hashmap

--- src/main/java/github/io/volong/Main.java
+++ src/main/java/github/io/volong/Main.java
@@ -3,7 +3,10 @@ package github.io.volong;
 public class Main {
 
     public static void main(String[] args) {
-        
-        System.out.println(16 >>> 2);
+
+    	System.out.println("11 % 8=" + (11 & 7));
+        System.out.println("11 % 4=" + (11 & 3));
+        System.out.println("11 % 2=" + (11 & 1));
+
     }
 }
