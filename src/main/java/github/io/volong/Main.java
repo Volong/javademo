00d1f9912a056a9da61ffeb950a49b279d134c13
Volong <yhlona523@gmail.com>
2018-09-27 23:37:33 +0800

    LongAdder

--- src/main/java/github/io/volong/Main.java
+++ src/main/java/github/io/volong/Main.java
@@ -1,17 +1,12 @@
 package github.io.volong;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		ScheduledExecutorService pool = Executors.newScheduledThreadPool(2);
-		
-		Task1 task1 = new Task1();
-		
-		pool.schedule(task1, 5, TimeUnit.SECONDS);//延迟5s后，执行且只执行一次task1
+		LongAdder longAdder = new LongAdder();
+		longAdder.add(2);
+		System.out.println(longAdder);
 	}
 }
