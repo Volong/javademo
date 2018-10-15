4c3bdd096805ec0f195c8a1b98983031d996c934
Volong <yhlona523@gmail.com>
2018-10-15 23:55:13 +0800

    StampedLock

--- src/main/java/github/io/volong/Main.java
+++ src/main/java/github/io/volong/Main.java
@@ -1,44 +1,11 @@
 package github.io.volong;
 
-import java.util.ArrayList;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.math.BigInteger;
 
 public class Main {
 
-    private static ArrayList<String> array = new ArrayList<String>();
-    //独占锁
-    private final static ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-    private final static Lock readLock = lock.readLock();
-    private final static Lock writeLock = lock.writeLock();
-
-    
-	public static void main(String[] args) throws InterruptedException {
-	    
-        new Thread(new Runnable() {
-            
-            @Override
-            public void run() {
-                writeLock.lock();
-                System.out.println("write already lock");
-                
-//                readLock.lock();
-//                System.out.println("read already lock");
-            }
-        }).start();
-        
-        Thread.sleep(1000);
-        
-        
-        
-        array.add("yi");
-
-	    readLock.lock();
-	    System.out.println("read1 already lock");
-	    
-	    readLock.lock();
-	    System.out.println("read2 already lock");
-	    
-	    System.out.println(array);
+	public static void main(String[] args) {
+		BigInteger bi = new BigInteger("11111111111111111111111110000000", 2);
+		System.out.println(Long.parseLong(bi.toString()));
 	}
 }
