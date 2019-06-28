2cbaf2b663f25756531ae020f226cd65b3fd3705
volong <yhlona523@gmail.com>
2019-06-28 09:12:16 +0800

    :smile:jvm

---
+++ src/main/java/github/io/volong/liblinear/Demo.java
@@ -0,0 +1,36 @@
+package github.io.volong.liblinear;
+
+import de.bwaldvogel.liblinear.*;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @since 2019-06-27 15:20
+ */
+public class Demo {
+
+    public static void main(String[] args) throws IOException {
+
+        Problem problem = new Problem();
+        problem.l = 1; // number of training examples
+        problem.n = 1; // number of features
+        // problem.x = ... // feature nodes
+        // problem.y = ... // target values
+
+        SolverType solver = SolverType.L2R_LR; // -s 0
+        double C = 1.0;    // cost of constraints violation
+        double eps = 0.01; // stopping criteria
+
+        Parameter parameter = new Parameter(solver, C, eps);
+        Model model = Linear.train(problem, parameter);
+        File modelFile = new File("model");
+        model.save(modelFile);
+
+        // load model or use it directly
+        model = Model.load(modelFile);
+
+        Feature[] instance = { new FeatureNode(1, 4), new FeatureNode(2, 2) };
+        double prediction = Linear.predict(model, instance);
+    }
+}
