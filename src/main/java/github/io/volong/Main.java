decafc3587f6afdd054b49c80a357e29f7aa00ff
Volong <yhlona523@gmail.com>
2019-07-09 21:43:33 +0800

    :smile:ThreadLocalRandom

--- src/main/java/github/io/volong/Main.java
+++ src/main/java/github/io/volong/Main.java
@@ -3,7 +3,9 @@ package github.io.volong;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.Random;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
@@ -22,11 +24,34 @@ public class Main {
         }
 
         public static void main(String[] args) {
-            Singleton instance = Singleton.getInstance();
-            System.out.println(instance);
 
-            Thread thread = new Thread();
-            thread.setDaemon(true);
+            ThreadLocalRandom mainCurrent = ThreadLocalRandom.current();
+            int i = mainCurrent.nextInt(5);
+            System.out.println("i:" + i);
+            int i1 = mainCurrent.nextInt(5);
+            System.out.println("i1:" + i1);
+
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    ThreadLocalRandom i2Current = ThreadLocalRandom.current();
+                    int i2 = i2Current.nextInt(5);
+                    System.out.println("i2:" + i2);
+                    System.out.println(i2Current == mainCurrent);
+                }
+            }).start();
+
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    ThreadLocalRandom i3Current = ThreadLocalRandom.current();
+                    int i3 = mainCurrent.nextInt(5);
+                    System.out.println("i3:" + i3);
+                    System.out.println(i3Current == mainCurrent);
+                }
+            }).start();
         }
     }
 }
