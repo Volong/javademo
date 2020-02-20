2465ea913d2d4654c6c17bd469cdfb6d3bd668df
YLongo <yhlona523@gmail.com>
2020-02-20 16:45:31 +0800

    :smile: bs

--- src/main/java/github/io/volong/Main.java
+++ src/main/java/github/io/volong/Main.java
@@ -8,30 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String str = "dfdffdsafas";
-
-//        if (str == null || str.length() == 0) {
-//            return 0;
-//        }
-
-        int count = 0;
-        int n = str.length();
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            for (int j = i; j < n; j++) {
-
-                if (i == j) { // 相邻的元素
-                    count++;
-                } else if (i + 1 == j && str.charAt(i) == str.charAt(j)) { // 隔了一个元素
-                    count++;
-                } else if (str.charAt(i) == str.charAt(j) && str.charAt(i + 1) == str.charAt(j - 1)) { // 隔了几个元素
-                    count++;
-                }
-            }
-        }
-
-        System.out.println(count);
+        System.out.println(4 ^ 6);
     }
 
 }
---
+++ src/main/java/github/io/volong/algorithm/BinarySearch.java
@@ -0,0 +1,32 @@
+package github.io.volong.algorithm;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(binarySearch(arr, 7));
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (arr[mid] < target) {
+                low = mid + 1;
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+
+        }
+
+        return -1;
+    }
+}
